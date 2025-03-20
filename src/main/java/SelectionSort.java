0e4c8207122803850eadc9fbd3d0a6f9c77807c4
stanley <coronavirusvaccinexx1@gmail.com>
2025-03-20 13:09:20 -0400

    implement selection sort

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -1,8 +1,23 @@
 class SelectionSort {
     public static void selectionSort(int[] arr) {
         int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int idx = i;
+            int j = i;
+            while (j < n) {
+                if (arr[j] < arr[idx]) {
+                    idx = j;
+                }
+                j++;
+            }
+            swap(arr, i, idx);
+        }
+    }
 
-       // STUDENT TODO
+    private static void swap(int[] arr, int i, int idx) {
+        int temp = arr[i];
+        arr[i] = arr[idx];
+        arr[idx] = temp;
     }
 
     public static void main(String[] args) {
